37afe63116c6d46aa38d349606a362625f3bc9ca
dretp <dp3.qwe@gmail.com>
2020-04-26 22:21:06 -0400

    Add files via upload
    snail project

---
+++ snail/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace snail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            snail Gray = new snail();
+            snail Larry = new snail();
+            //setting snail name
+            Gray.Name = "gary";
+            Larry.Name = "Larry";
+            //setting snail length
+           Gray.length = 6;
+           Larry.length = 8;
+
+            //setting snail age 
+                Gray.age = 12;
+                Larry.age = 16;
+
+            Console.WriteLine(Gray.Name);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ snail/snail.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace snail
+{
+    class snail
+    {
+        int snail_length;
+        string snail_name;
+        int snail_age;
+
+         // property
+  
+               public int length {  // property
+        
+            get { return snail_length; }
+            set { snail_length = value; }
+                } 
+              public string Name {  // property
+        
+            get { return snail_name; }
+            set { snail_name = value; }
+        } 
+        
+              public int age {  // property
+        
+            get { return snail_age; }
+            set { snail_age = value; }
+                } 
+
+        
+    }
+
+}
